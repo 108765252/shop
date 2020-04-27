7adb9b123c09ef883cf738b45fe692dd3e3ae5b9
108765252@qq.com <108765252@qq.com>
2020-04-27 17:02:35 +0800

    17:02

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
   <div id="app">
+    <router-view></router-view>
     <ul class="footer-bar">
-      <li class="footer-bar-item">首页</li>
-      <li class="footer-bar-item">分类</li>
-      <li class="footer-bar-item">购物车</li>
-      <li class="footer-bar-item">我的</li>
+      <li class="footer-bar-item">
+        <router-link to="/">首页</router-link>
+        </li>
+      <li class="footer-bar-item">
+         <router-link to="/Category">分类</router-link>
+         </li>
+      <li class="footer-bar-item">
+        <router-link to="/Cart">购物车</router-link>
+        </li>
+      <li class="footer-bar-item">
+        <router-link to="/Profile">我的</router-link>
+        </li>
     </ul>
   </div>
 </template>
@@ -15,7 +24,8 @@
   background: #eeeeee;
   position: fixed;
   width: 100%;
-  height: 50px;
+  height: 1rem;
+  line-height: 1rem;
   bottom: 0;
   &-item {
     flex-grow: 1;

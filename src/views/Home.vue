7ac091d4338147d08984fb56e1be991529116c8b
108765252 <108765252@qq.com>
2020-04-28 17:48:39 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,27 +7,66 @@
 </van-nav-bar>
 
 <!--轮播图-->
-
-<van-swipe class="my-swipe" :autoplay="3000" indicator-color="white">
-  <van-swipe-item>1</van-swipe-item>
-  <van-swipe-item>2</van-swipe-item>
-  <van-swipe-item>3</van-swipe-item>
-  <van-swipe-item>4</van-swipe-item>
+<div class="carousel">
+<van-swipe :autoplay="3000">
+  <van-swipe-item class="carousel-item" v-for="(item,index) in carouselItem" :key="index">
+ <img :src="item.imgSrc" alt="">
+  </van-swipe-item>
 </van-swipe>
+</div>
 
-    
 
+<input type="text" id="inp" />
+你说啥: <span id="demo"></span>
 
 </div>
+
   
 </template>
 
 <script>
 
-export default{
+//export default{
+//     // data数据返回数组
+// data(){
+//     return{
+//         carouselItem: [
+//             {
+//                 name:'img1',
+//                 imgSrc:'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1041994715,1243354266&fm=26&gp=0.jpg'
+//             },
+//              {
+//                 name:'img2',
+//                 imgSrc:'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=116744544,2055858210&fm=26&gp=0.jpg'
+//             },
+//              {
+//                 name:'img3',
+//                 imgSrc:'http://linyi.dzwww.com/focus/202004/W020200406281461395731.jpg'
+//             }
+
+//         ]
+//     }
+    
+  var Obj = {}// 监听对象
+  var demo = document.getElementById('demo') // 获取DOM
+  var inp = document.getElementById('inp') // 获取DOM
+  Object.defineProperty(Obj, 'text', {
+    configurable: true,
+    enumerable: true,
+    set: function (val) {
+      // 将Obj对象新改变的值赋值给input的value，然后页赋值给demo
+      inp.value = val
+      demo.innerHTML = val
+      console.log('input里面输入了' + val)
+    }
+  })
+    inp.addEventListener('keyup',function(e){
+        // console.log(e.target.value)
+        Obj.text = e.target.value
+    }
+    )
 
 
-}
 </script>
 
 <style lang="scss">

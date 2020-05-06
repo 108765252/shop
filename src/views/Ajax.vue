2e15c2b64f879e8369788bed5e83c4bb0afcb302
108765252 <108765252@qq.com>
2020-05-06 13:58:14 +0800

    1

--- src/views/Ajax.vue
+++ src/views/Ajax.vue
@@ -2,11 +2,10 @@
   <div>
     ajax
     <van-button type="primary" @click="getData">获取数据</van-button>
-    <van-button type="danger">清空数据</van-button>
+    <van-button type="danger" @click="clearData">清空数据</van-button>
     <ul>
       <li v-for="(item,index) in list" :key="index">
-          {{index+1}}: {{item.title}}
-          </li>
+        {{index+1}}: {{item.title}}</li>
     </ul>
   </div>
 </template>
@@ -15,33 +14,35 @@
 export default {
   data() {
     return {
-      //应该返回对象
       list: []
     };
   },
   methods: {
+    clearData(){
+    this.list = [];
+  },
     ajax() {
       //ajax原理
-
       let movieUrl =
         "https://douban.uieee.com/v2/movie/top250?start=0&count=15";
-
-      //创建XMLHttpRequest对象 从地址获取数据
-      // eslint-disable-next-line no-unused-vars
+      // 创建对象
       let xmlhttp;
       if (window.XMLHttpRequest) {
         xmlhttp = new XMLHttpRequest();
       } else {
         // eslint-disable-next-line no-undef
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        xmlhttp.open("GET", movieUrl, true);
-        xmlhttp.send();
       }
+      //初始化 HTTP 请求参数，例如 URL 和 HTTP 方法GET、POST、HEAD，true是异步，但是并不发送请求。
+        xmlhttp.open("GET", movieUrl, true);
+        //通过调用上面的open()参数指定发送一个 HTTP 请求
+        xmlhttp.send();
+      
       xmlhttp.addEventListener("readystatechange", () => {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
           let obj = JSON.parse(xmlhttp.responseText);
           console.log(obj);
-          //this.list = obj.subjects;
+          this.list = obj.subjects;
           // ES6语法：this在这个作用域中list定义渲染对象内容
         }
       });
